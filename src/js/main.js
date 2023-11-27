6e2670455aa91ade1f8303ab6ceb1ade1601bcf2
sashasashkina <koshyl.apps@gmail.com>
2023-11-27 15:51:53 +0200

    committen

--- src/js/main.js
+++ src/js/main.js
@@ -12,120 +12,65 @@ const btnLodeMore = document.querySelector('.load-more');
 
 let page = 1;
 let searchName = '';
-let perPage = 40;
 
 let lightbox = new SimpleLightbox('.gallery-link', {
   captionDelay: 250,
   captionsData: 'alt',
 });
-// function handleSubmit(e) {
-//   e.preventDefault();
-
-//   page = 1;
-//   galleryImage.innerHTML = '';
-
-//   searchName = searchInput.value.trim();
-
-//   getImages(searchName, page)
-//     .then(({ data: { hits, totalHits } }) => {
-//       if (hits.length === 0) {
-//         throw new Error();
-//       }
-//       createMarkUp(hits);
-//       btnLodeMore.removeAttribute('hidden');
-
-//       Notify.success(`Hooray! We found ${totalHits} images.`);
-//       lightbox.refresh();
-//     })
-
-//     .catch(() =>
-//       Notify.failure(
-//         `Sorry, there are no images matching your search query. Please try again.`
-//       )
-//     )
-//     .finally(() => {
-//       e.target.reset();
-//     });
-// }
-
-// function handleClick(event) {
-//   btnLodeMore.setAttribute('hidden', true);
-
-//   page += 1;
-
-//   getImages(searchName, page)
-//     .then(({ data: { hits, totalHits } }) => {
-//       if (hits.length === 0) {
-//         btnLodeMore.setAttribute('hidden', true);
-//         Notify.info(
-//           `We're sorry, but you've reached the end of search results.`
-//         );
-//         return;
-//       }
-//       createMarkUp(hits);
-//       btnLodeMore.removeAttribute('hidden');
-//       lightbox.refresh();
-//     })
-//     .catch(error => console.log(error));
-// }
-
-// searchForm.addEventListener('submit', handleSubmit);
-// btnLodeMore.addEventListener('click', handleClick);
 async function handleSubmit(e) {
   e.preventDefault();
-
   page = 1;
   galleryImage.innerHTML = '';
 
+  if (!searchName) {
+    return;
+  }
   searchName = searchInput.value.trim();
-
-  try{
-     const galleryItems = await getImages(searchName, page);
-
-       
-     if(({ data: { hits, totalHits } }) => {
-      if (hits.length === 0) {
-        throw new Error();
-      }
-      createMarkUp(hits);
+  try {
+    const images = await getImages(searchName, page);
+    const {
+      data: { hits, totalHits },
+    } = images;
+    if (hits.length === 0) {
+      throw new Error();
+    }
+    if (page >= Math.ceil(totalHits / 40)) {
+      btnLodeMore.setAttribute('hidden', true);
+      Notify.info(`We're sorry, but you've reached the end of search results.`);
+    } else {
       btnLodeMore.removeAttribute('hidden');
-
-      Notify.success(`Hooray! We found ${totalHits} images.`);
-      lightbox.refresh();
-    })
-
-    }catch(error){
-      Notify.failure(
-        `Sorry, there are no images matching your search query. Please try again.`
-      )}
-    
-    .finally(){
-      e.target.reset();
-    };
-
-
-async function handleClick(event) {
+    }
+    createMarkUp(hits);
+    Notify.success(`Hooray! We found ${totalHits} images.`);
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error);
+    Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
+  } finally {
+    e.target.reset();
+  }
+}
+async function handleClick() {
   btnLodeMore.setAttribute('hidden', true);
-
   page += 1;
-
- try{
-
-  const galleryItems = getImages(searchName, page)
-    .then(({ data: { hits, totalHits } }) => {
-      if (hits.length === 0) {
-        btnLodeMore.setAttribute('hidden', true);
-        Notify.info(
-          `We're sorry, but you've reached the end of search results.`
-        );
-        return;
-      }
-      createMarkUp(hits);
+  try {
+    const images = await getImages(searchName, page);
+    const {
+      data: { hits, totalHits },
+    } = images;
+    if (page >= Math.ceil(totalHits / 40)) {
+      btnLodeMore.setAttribute('hidden', true);
+      Notify.info(`We're sorry, but you've reached the end of search results.`);
+    } else {
       btnLodeMore.removeAttribute('hidden');
-      lightbox.refresh();
-    })
-    }catch(error ){
-     console.log(error)};
+    }
+    createMarkUp(hits);
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 searchForm.addEventListener('submit', handleSubmit);
